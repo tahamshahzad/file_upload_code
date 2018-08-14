9b68d7fcad7763abb8b410e23d9dcb16ab30f07a
taha shahzad <taha.shahzad.0728@gmail.com>
2018-08-15 00:44:28 +0500

    new commit

---
+++ routes/upload.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const multer = require('multer');
+const upload = multer({ dest : 'uploads/' });
+
+var router = express.Router();
+
+/* GET home page. */
+router.post('/', upload.single("fileNew"), function(req, res, next) {
+   res.json("file uploadd");
+});
+
+module.exports = router;
